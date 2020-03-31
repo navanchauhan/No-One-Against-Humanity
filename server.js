33ec7057d9fe6edde25588c4ec526dddda25a143
Navan Chauhan <navanchauhan@gmail.com>
2020-03-31 15:56:24 +0530

    started imgrating express from 3 to 4

--- server.js
+++ server.js
@@ -5,14 +5,16 @@ var Game = require('./game.js')
 var players = { };
 var io = require('socket.io').listen(server);
 var _ = require('underscore');
+var bodyParser = require('body-parser')
+var methodOverride = require('method-override')
 
 server.listen(process.env.PORT || 3000);
 
 app.set('view engine', 'ejs');
 app.set('view options', { layout: false });
-app.use(express.methodOverride());
-app.use(express.bodyParser());  
-app.use(app.router);
+app.use(bodyParser());  
+app.use(methodOverride());
+//app.use(app.router);
 app.use('/public', express.static('public'));
 
 function json(o, res) {
